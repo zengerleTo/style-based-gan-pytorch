5eb32d3878065203c90c7d6ae25b463eb26b3612
zengerleTUM <tobi.zengerle@gmail.com>
2020-05-29 01:31:24 +0200

    implemented encoder trainig

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ from torch.utils.data import DataLoader
 from torchvision import datasets, transforms, utils
 
 from dataset import MultiResolutionDataset
-from model import StyledGenerator, Discriminator
+from model import StyledGenerator, Discriminator, Encoder
 
 
 def requires_grad(model, flag=True):
@@ -43,54 +43,35 @@ def adjust_lr(optimizer, lr):
         group['lr'] = lr * mult
 
 
-def train(args, dataset, generator, discriminator):
-    step = int(math.log2(args.init_size)) - 2
-    resolution = 4 * 2 ** step
+def train(args, dataset, encoder, generator, discriminator):
+    step=5
+    resolution = 128
     loader = sample_data(
         dataset, args.batch.get(resolution, args.batch_default), resolution
     )
     data_loader = iter(loader)
 
-    adjust_lr(g_optimizer, args.lr.get(resolution, 0.001))
+    adjust_lr(e_optimizer, args.lr.get(resolution, 0.001))
     adjust_lr(d_optimizer, args.lr.get(resolution, 0.001))
 
-    pbar = tqdm(range(3_000_000))
+    pbar = tqdm(range(560_000))
 
+    requires_grad(encoder, True)
     requires_grad(generator, False)
     requires_grad(discriminator, True)
 
     disc_loss_val = 0
-    gen_loss_val = 0
+    enc_loss_val = 0
     grad_loss_val = 0
 
-    alpha = 0
+    alpha = 1
     used_sample = 0
-
-    max_step = int(math.log2(args.max_size)) - 2
-    final_progress = False
+    epoch = 0
 
     for i in pbar:
-        discriminator.zero_grad()
-
-        alpha = min(1, 1 / args.phase * (used_sample + 1))
-
-        if (resolution == args.init_size and args.ckpt is None) or final_progress:
-            alpha = 1
-
-        if used_sample > args.phase * 2:
+        if used_sample > args.phase:
             used_sample = 0
-            step += 1
-
-            if step > max_step:
-                step = max_step
-                final_progress = True
-                ckpt_step = step + 1
-
-            else:
-                alpha = 0
-                ckpt_step = step
-
-            resolution = 4 * 2 ** step
+            epoch += 1
 
             loader = sample_data(
                 dataset, args.batch.get(resolution, args.batch_default), resolution
@@ -99,18 +80,18 @@ def train(args, dataset, generator, discriminator):
 
             torch.save(
                 {
+                    'encoder': encoder.state_dict(),
                     'generator': generator.module.state_dict(),
                     'discriminator': discriminator.module.state_dict(),
-                    'g_optimizer': g_optimizer.state_dict(),
+                    'e_optimizer': e_optimizer.state_dict(),
                     'd_optimizer': d_optimizer.state_dict(),
-                    'g_running': g_running.state_dict(),
                 },
-                f'checkpoint/train_step-{ckpt_step}.model',
+                f'checkpoint/epoch-{epoch}.model',
             )
 
-            adjust_lr(g_optimizer, args.lr.get(resolution, 0.001))
+            adjust_lr(e_optimizer, args.lr.get(resolution, 0.001))
             adjust_lr(d_optimizer, args.lr.get(resolution, 0.001))
-
+            
         try:
             real_image = next(data_loader)
 
@@ -123,98 +104,104 @@ def train(args, dataset, generator, discriminator):
         b_size = real_image.size(0)
         real_image = real_image.cuda()
 
+        #discriminator training
+#        discriminator.zero_grad()
+#
+#        if args.loss == 'wgan-gp':
+#            real_predict = discriminator(real_image, step=step, alpha=alpha)
+#            real_predict = real_predict.mean() - 0.001 * (real_predict ** 2).mean()
+#            (-real_predict).backward()
+#
+#        elif args.loss == 'r1':
+#            real_image.requires_grad = True
+#            real_scores = discriminator(real_image, step=step, alpha=alpha)
+#            real_predict = F.softplus(-real_scores).mean()
+#            real_predict.backward(retain_graph=True)
+#
+#            grad_real = grad(
+#                outputs=real_scores.sum(), inputs=real_image, create_graph=True
+#            )[0]
+#            grad_penalty = (
+#                grad_real.view(grad_real.size(0), -1).norm(2, dim=1) ** 2
+#            ).mean()
+#            grad_penalty = 10 / 2 * grad_penalty
+#            grad_penalty.backward()
+#            if i%10 == 0:
+#                grad_loss_val = grad_penalty.item()
+#
+#        if args.mixing and random.random() < 0.9:
+#            gen_in11, gen_in12, gen_in21, gen_in22 = torch.randn(
+#                4, b_size, code_size, device='cuda'
+#            ).chunk(4, 0)
+#            gen_in1 = [gen_in11.squeeze(0), gen_in12.squeeze(0)]
+#            gen_in2 = [gen_in21.squeeze(0), gen_in22.squeeze(0)]
+#
+#        else:
+#            gen_in1, gen_in2 = torch.randn(2, b_size, code_size, device='cuda').chunk(
+#                2, 0
+#            )
+#            gen_in1 = gen_in1.squeeze(0)
+#            gen_in2 = gen_in2.squeeze(0)
+#
+#        fake_image = generator(gen_in1, step=step, alpha=alpha)
+#        fake_predict = discriminator(fake_image, step=step, alpha=alpha)
+#
+#        if args.loss == 'wgan-gp':
+#            fake_predict = fake_predict.mean()
+#            fake_predict.backward()
+#
+#            eps = torch.rand(b_size, 1, 1, 1).cuda()
+#            x_hat = eps * real_image.data + (1 - eps) * fake_image.data
+#            x_hat.requires_grad = True
+#            hat_predict = discriminator(x_hat, step=step, alpha=alpha)
+#            grad_x_hat = grad(
+#                outputs=hat_predict.sum(), inputs=x_hat, create_graph=True
+#            )[0]
+#            grad_penalty = (
+#                (grad_x_hat.view(grad_x_hat.size(0), -1).norm(2, dim=1) - 1) ** 2
+#            ).mean()
+#            grad_penalty = 10 * grad_penalty
+#            grad_penalty.backward()
+#            if i%10 == 0:
+#                grad_loss_val = grad_penalty.item()
+#                disc_loss_val = (-real_predict + fake_predict).item()
+#
+#        elif args.loss == 'r1':
+#            fake_predict = F.softplus(fake_predict).mean()
+#            fake_predict.backward()
+#            if i%10 == 0:
+#                disc_loss_val = (real_predict + fake_predict).item()
+#
+#        d_optimizer.step()
+
+        #Encoder training
+        encoder.zero_grad()
+
+        requires_grad(encoder, True)
+        requires_grad(discriminator, False)
+
+        latent_w = encoder(real_image)
+        
+        embedded_image = generator(latent_w, step=step, alpha=alpha)
+
+        predict = discriminator(embedded_image, step=step, alpha=alpha)
+
         if args.loss == 'wgan-gp':
-            real_predict = discriminator(real_image, step=step, alpha=alpha)
-            real_predict = real_predict.mean() - 0.001 * (real_predict ** 2).mean()
-            (-real_predict).backward()
+            loss = -predict.mean()
 
         elif args.loss == 'r1':
-            real_image.requires_grad = True
-            real_scores = discriminator(real_image, step=step, alpha=alpha)
-            real_predict = F.softplus(-real_scores).mean()
-            real_predict.backward(retain_graph=True)
+            loss = F.softplus(-predict).mean()
+            
+        loss += nn.MSELoss(real_image, embedded_image)
 
-            grad_real = grad(
-                outputs=real_scores.sum(), inputs=real_image, create_graph=True
-            )[0]
-            grad_penalty = (
-                grad_real.view(grad_real.size(0), -1).norm(2, dim=1) ** 2
-            ).mean()
-            grad_penalty = 10 / 2 * grad_penalty
-            grad_penalty.backward()
-            if i%10 == 0:
-                grad_loss_val = grad_penalty.item()
+        if i%10 == 0:
+            enc_loss_val = loss.item()
 
-        if args.mixing and random.random() < 0.9:
-            gen_in11, gen_in12, gen_in21, gen_in22 = torch.randn(
-                4, b_size, code_size, device='cuda'
-            ).chunk(4, 0)
-            gen_in1 = [gen_in11.squeeze(0), gen_in12.squeeze(0)]
-            gen_in2 = [gen_in21.squeeze(0), gen_in22.squeeze(0)]
+        loss.backward()
+        e_optimizer.step()
 
-        else:
-            gen_in1, gen_in2 = torch.randn(2, b_size, code_size, device='cuda').chunk(
-                2, 0
-            )
-            gen_in1 = gen_in1.squeeze(0)
-            gen_in2 = gen_in2.squeeze(0)
-
-        fake_image = generator(gen_in1, step=step, alpha=alpha)
-        fake_predict = discriminator(fake_image, step=step, alpha=alpha)
-
-        if args.loss == 'wgan-gp':
-            fake_predict = fake_predict.mean()
-            fake_predict.backward()
-
-            eps = torch.rand(b_size, 1, 1, 1).cuda()
-            x_hat = eps * real_image.data + (1 - eps) * fake_image.data
-            x_hat.requires_grad = True
-            hat_predict = discriminator(x_hat, step=step, alpha=alpha)
-            grad_x_hat = grad(
-                outputs=hat_predict.sum(), inputs=x_hat, create_graph=True
-            )[0]
-            grad_penalty = (
-                (grad_x_hat.view(grad_x_hat.size(0), -1).norm(2, dim=1) - 1) ** 2
-            ).mean()
-            grad_penalty = 10 * grad_penalty
-            grad_penalty.backward()
-            if i%10 == 0:
-                grad_loss_val = grad_penalty.item()
-                disc_loss_val = (-real_predict + fake_predict).item()
-
-        elif args.loss == 'r1':
-            fake_predict = F.softplus(fake_predict).mean()
-            fake_predict.backward()
-            if i%10 == 0:
-                disc_loss_val = (real_predict + fake_predict).item()
-
-        d_optimizer.step()
-
-        if (i + 1) % n_critic == 0:
-            generator.zero_grad()
-
-            requires_grad(generator, True)
-            requires_grad(discriminator, False)
-
-            fake_image = generator(gen_in2, step=step, alpha=alpha)
-
-            predict = discriminator(fake_image, step=step, alpha=alpha)
-
-            if args.loss == 'wgan-gp':
-                loss = -predict.mean()
-
-            elif args.loss == 'r1':
-                loss = F.softplus(-predict).mean()
-
-            if i%10 == 0:
-                gen_loss_val = loss.item()
-
-            loss.backward()
-            g_optimizer.step()
-            accumulate(g_running, generator.module)
-
-            requires_grad(generator, False)
-            requires_grad(discriminator, True)
+        requires_grad(encoder, False)
+        requires_grad(discriminator, True)
 
         if (i + 1) % 100 == 0:
             images = []
@@ -224,8 +211,8 @@ def train(args, dataset, generator, discriminator):
             with torch.no_grad():
                 for _ in range(gen_i):
                     images.append(
-                        g_running(
-                            torch.randn(gen_j, code_size).cuda(), step=step, alpha=alpha
+                        generator(
+                            encoder(real_image), step=step, alpha=alpha
                         ).data.cpu()
                     )
 
@@ -237,13 +224,8 @@ def train(args, dataset, generator, discriminator):
                 range=(-1, 1),
             )
 
-        if (i + 1) % 10000 == 0:
-            torch.save(
-                g_running.state_dict(), f'checkpoint/{str(i + 1).zfill(6)}.model'
-            )
-
         state_msg = (
-            f'Size: {4 * 2 ** step}; G: {gen_loss_val:.3f}; D: {disc_loss_val:.3f};'
+            f'Size: {4 * 2 ** step}; G: {enc_loss_val:.3f}; D: {disc_loss_val:.3f};'
             f' Grad: {grad_loss_val:.3f}; Alpha: {alpha:.5f}'
         )
 
@@ -261,7 +243,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '--phase',
         type=int,
-        default=600_000,
+        default=56_000,
         help='number of samples used for each training phases',
     )
     parser.add_argument('--lr', default=0.001, type=float, help='learning rate')
@@ -286,37 +268,40 @@ if __name__ == '__main__':
         choices=['wgan-gp', 'r1'],
         help='class of gan loss',
     )
+    parser.add_argument('--gen_path', default='', type=str, help='path of the pretrained generator')
+    parser.add_argument('--discr_path', default='',type=str, help='path of the pretrained generator')
 
     args = parser.parse_args()
 
+    encoder = nn.DataParallel(Encoder(size=128, filters=64, filters_max=512, num_layers=1)).cuda()
     generator = nn.DataParallel(StyledGenerator(code_size)).cuda()
     discriminator = nn.DataParallel(
         Discriminator(from_rgb_activate=not args.no_from_rgb_activate)
     ).cuda()
-    g_running = StyledGenerator(code_size).cuda()
-    g_running.train(False)
 
-    g_optimizer = optim.Adam(
+    e_optimizer = optim.Adam(
         generator.module.generator.parameters(), lr=args.lr, betas=(0.0, 0.99)
     )
-    g_optimizer.add_param_group(
+    e_optimizer.add_param_group(
         {
-            'params': generator.module.style.parameters(),
+            'params': encoder.module.style.parameters(),
             'lr': args.lr * 0.01,
             'mult': 0.01,
         }
     )
     d_optimizer = optim.Adam(discriminator.parameters(), lr=args.lr, betas=(0.0, 0.99))
 
-    accumulate(g_running, generator.module, 0)
+    if args.gen_path != '':
+        generator.module.load_state_dict(args.gen_path)
+    if args.discr_path != '':
+        generator.module.load_state_dict(args.discr_path['discriminator'])
 
     if args.ckpt is not None:
         ckpt = torch.load(args.ckpt)
 
-        generator.module.load_state_dict(ckpt['generator'])
+        encoder.load_state_dict(ckpt['encoder'])
         discriminator.module.load_state_dict(ckpt['discriminator'])
-        g_running.load_state_dict(ckpt['g_running'])
-        g_optimizer.load_state_dict(ckpt['g_optimizer'])
+        e_optimizer.load_state_dict(ckpt['e_optimizer'])
         d_optimizer.load_state_dict(ckpt['d_optimizer'])
 
     transform = transforms.Compose(
@@ -341,4 +326,4 @@ if __name__ == '__main__':
 
     args.batch_default = 32
 
-    train(args, dataset, generator, discriminator)
+    train(args, dataset, encoder, generator, discriminator)
